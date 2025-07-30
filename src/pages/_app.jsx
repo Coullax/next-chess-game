050810ab8bf97185b8714c76be80e18b1a3d69e4
ushansankalpa <ushansan99@gmail.com>
2025-07-30 12:10:46 +0530

    fixed issues

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,6 +12,15 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <script src="https://code.jquery.com/jquery-3.7.0.min.js" />
       </Head>
+      <Script
+        src="/js/jquery-3.7.0.min.js"
+        strategy="beforeInteractive"
+        // onLoad={() => {
+        //   console.log("jQuery loaded");
+        //   setScriptsReady(false); // Reset to ensure next script waits
+        // }}
+        // onError={() => console.error("jQuery failed to load")}
+      />
 
       <Component {...pageProps} />
     </>
