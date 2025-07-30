1343a98b4a30d679626c9742cad9ddfe62a1b9b1
Madhawa Awishka <madhawaawishka@gmail.com>
2025-07-30 18:04:53 +0530

    asset poremix removed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './', // Ensures relative paths work
+  // assetPrefix: './', // Ensures relative paths work
   images: {
     unoptimized: true, // Disable image optimization if using static export
   },
