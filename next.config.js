f81f22cd4f80f1681769b750ad9e3856215a1d48
ushansankalpa <ushansan99@gmail.com>
2025-07-30 13:22:03 +0530

    fixed issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  assetPrefix: './', // Ensures relative paths work
+  images: {
+    unoptimized: true, // Disable image optimization if using static export
+  },
 }
 
 module.exports = nextConfig
